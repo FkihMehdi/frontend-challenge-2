a3545d7e693452a7cbfc4f98b7c9307474fa3323
FkihMehdi <mehdi.fkih@insat.ucar.tn>
2025-04-29 17:06:51 +0100

    feat: add a test for the app component

--- apps/frontend-challenge/src/app/app.spec.tsx
+++ apps/frontend-challenge/src/app/app.spec.tsx
@@ -1,9 +1,49 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import App from './app';
+import { RecoilRoot } from 'recoil';
+import { MessageProvider } from '../context/MessageContext';
 import '@testing-library/jest-dom';
 
-describe('App', () => {
-  it('can pass this test', () => {
-    render(<div>Test</div>);
-    expect(true).toBe(true);
+jest.mock('../components/Header', () => () => <div>Header Component</div>);
+jest.mock('../components/Controls', () => () => <div>Controls Component</div>);
+jest.mock('../components/MessageTable', () => () => (
+  <div>Message Table Component</div>
+));
+jest.mock('../components/ErrorSnackbar', () => () => (
+  <div>Error Snackbar Component</div>
+));
+
+describe('App Component', () => {
+  it('renders without crashing', () => {
+    render(
+      <RecoilRoot>
+        <MessageProvider>
+          <App />
+        </MessageProvider>
+      </RecoilRoot>
+    );
+
+    expect(screen.getByText('Header Component')).toBeInTheDocument();
+
+    expect(screen.getByText('Controls Component')).toBeInTheDocument();
+
+    expect(screen.getByText('Message Table Component')).toBeInTheDocument();
+
+    expect(screen.getByText('Error Snackbar Component')).toBeInTheDocument();
+  });
+
+  it('renders all necessary components inside MessageProvider and RecoilRoot', () => {
+    render(
+      <RecoilRoot>
+        <MessageProvider>
+          <App />
+        </MessageProvider>
+      </RecoilRoot>
+    );
+
+    expect(screen.getByText('Header Component')).toBeInTheDocument();
+    expect(screen.getByText('Controls Component')).toBeInTheDocument();
+    expect(screen.getByText('Message Table Component')).toBeInTheDocument();
+    expect(screen.getByText('Error Snackbar Component')).toBeInTheDocument();
   });
 });
