825feb27955bea5045e48560a96cd34fa46c4ed2
FkihMehdi <mehdi.fkih@insat.ucar.tn>
2025-04-29 11:23:03 +0100

    fix: change the getMessageBgColor to utilities

--- apps/frontend-challenge/src/components/MessageTable/MessageItem.tsx
+++ apps/frontend-challenge/src/components/MessageTable/MessageItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Message, Priority } from '@frontend-challenge/types';
+import { Message } from '@frontend-challenge/types';
+import { getMessageBgColor } from '@frontend-challenge/utilities';
 import { X } from 'lucide-react';
 
 interface MessageItemProps {
@@ -7,22 +8,11 @@ interface MessageItemProps {
 }
 
 const MessageItem: React.FC<MessageItemProps> = ({ message }) => {
-  const getBgColor = () => {
-    switch (message.priority) {
-      case Priority.Error:
-        return 'bg-priority-error/10 border-priority-error';
-      case Priority.Warning:
-        return 'bg-priority-warning/10 border-priority-warning';
-      case Priority.Info:
-        return 'bg-priority-info/10 border-priority-info';
-      default:
-        return 'bg-gray-100 border-gray-300';
-    }
-  };
-
   return (
     <div
-      className={`relative p-3 mb-2 rounded-md border-l-4 shadow-sm ${getBgColor()} transition-all hover:shadow-md group`}
+      className={`relative p-3 mb-2 rounded-md border-l-4 shadow-sm ${getMessageBgColor(
+        message.priority
+      )} transition-all hover:shadow-md group`}
     >
       <button
         data-id={message.id}
--- libs/utilities/src/lib/utilities.ts
+++ libs/utilities/src/lib/utilities.ts
@@ -1,3 +1,5 @@
+import { Priority } from '@frontend-challenge/types';
+
 export function areEqual<T extends object>(
   prevProps: T,
   nextProps: T,
@@ -5,3 +7,16 @@ export function areEqual<T extends object>(
 ): boolean {
   return keys.every((key) => prevProps[key] === nextProps[key]);
 }
+
+export const getMessageBgColor = (priority: Priority): string => {
+  switch (priority) {
+    case Priority.Error:
+      return 'bg-priority-error/10 border-priority-error';
+    case Priority.Warning:
+      return 'bg-priority-warning/10 border-priority-warning';
+    case Priority.Info:
+      return 'bg-priority-info/10 border-priority-info';
+    default:
+      return 'bg-gray-100 border-gray-300';
+  }
+};
